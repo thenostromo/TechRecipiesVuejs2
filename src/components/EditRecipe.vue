ed03401b0b22990ba3cbcaf221587d49122f9686
Alexander <redlesleys@gmail.com>
2019-08-21 22:41:13 +0300

    Develop

--- src/components/EditRecipe.vue
+++ src/components/EditRecipe.vue
@@ -1,31 +1,111 @@
 <template>
   <div v-if="recipe" class="edit-recipe container">
     <h2>Edit {{ recipe.title }} Recipe</h2>
+    <form @submit.prevent="editRecipe">
+      <div class="field title">
+        <label for="title">Recipe title:</label>
+        <input type="text" name="title" v-model="recipe.title">
+      </div>
+      <div v-for="(keyword, index) in recipe.keywords" :key="index" class="field">
+        <label for="keyword">Keyword:</label>
+        <input type="text" name="keyword" v-model="recipe.keywords[index]">
+        <i class="material-icons delete" @click="deleteKeyword(keyword)">delete</i>
+      </div>
+      <div class="field edit-recipe">
+        <label for="add-recipe">Add a keyword:</label>
+        <input type="text" name="add-recipe"
+               @keydown.tab.prevent="addKeyword"
+               v-model="keyword">
+      </div>
+      <div class="field center-align">
+        <p v-if="feedback" class="red-text">{{ feedback }}</p>
+        <button class="btn pink">Update Recipe</button>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
   import db from '@/firebase/init'
+  import slugify from 'slugify'
 
   export default {
     name: "EditRecipe",
     data() {
       return {
-        recipe: null
+        recipe: null,
+        keyword: null,
+        feedback: null
       }
     },
     created() {
-      let ref = db.collection('recipies').where('slug', '==', this.$router.params.recipe_slug)
+      let ref = db.collection('recipies').where('slug', '==', this.$router.currentRoute.params.recipe_slug)
       ref.get().then(snapshot => {
         snapshot.forEach(doc => {
           this.recipe = doc.data()
           this.recipe.id = doc.id
         })
       })
+    },
+    methods: {
+      editRecipe() {
+        if (this.recipe.title) {
+          this.feedback = null
+          this.recipe.slug = slugify(this.recipe.title, {
+            replacement: '-',
+            remove: /[$*_+~.()'"!\-:@]/g,
+            lower: true
+          })
+          db.collection('recipies').doc(recipe.id).update({
+            title: this.recipe.title,
+            keywords: this.recipe.keywords,
+            slug: this.recipe.slug
+          }).then(() => {
+            this.$router.push({ name: 'Index' })
+          }).catch(err => {
+            console.log(err)
+          })
+        } else {
+          this.feedback = 'You must enter a recipe title'
+        }
+      },
+      addKeyword() {
+        if (this.keyword) {
+          this.recipe.keywords.push(this.keyword)
+          this.keyword = null
+          this.feedback = null
+        } else {
+          this.feedback = 'You must enter a value to add a keyword'
+        }
+      },
+      deleteKeyword(keyword) {
+        this.recipe.keywords = this.recipe.keywords.filter(found => {
+          return found != keyword
+        })
+      }
     }
   }
 </script>
 
 <style>
-
+  .edit-recipe {
+    margin-top: 60px;
+    max-width: 500px;
+  }
+  .edit-recipe h2 {
+    font-size: 2em;
+    margin: 20px auto;
+  }
+  .edit-recipe .field {
+    margin: 20px auto;
+    position: relative;
+  }
+  .edit-recipe .delete {
+    position: absolute;
+    right: 0;
+    bottom: 16px;
+    color: #aaa;
+    font-size: 1.4em;
+    cursor: pointer;
+  }
 </style>
